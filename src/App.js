3c20cc7251f6af42cd05448a2297c6ba185d25e9
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-10-03 09:28:12 +0200

    Add perspectiv bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import PerspectiveBar from "./components/PerspectiveBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <PerspectiveBar />
       <Footer />
     </>
   );
---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,5 @@
+import styles from "./PerspectiveBar.module.css";
+
+export default function PerspectiveBar() {
+  return <div className={styles.perspectiveBar}></div>;
+}
